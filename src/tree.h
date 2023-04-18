5a793baa89792fcb5536419b16e6cfdd63451eee
maximalekseenko <max.alekseenko@gmail.com>
2023-04-18 09:17:40 +0300

    tree ... fix

--- src/tree.h
+++ src/tree.h
@@ -7,7 +7,7 @@
 #include <regex>
 
 struct Tree : public TreeCore<Tree> { 
-    Tree (int __value, ...) : TreeCore(__value) { }
+    Tree (int __value) : TreeCore(__value) { }
     void Update(){}
     std::string ToString() { return std::to_string(__get_value()); }
 };
